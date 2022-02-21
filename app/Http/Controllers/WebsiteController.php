9542d9cd99bf028af8f27b5e9f9f445b10bda998
Hiren <hiren@freshbits.in>
2022-02-21 16:26:06 +0530

    Add endpoints for the websites

---
+++ app/Http/Controllers/WebsiteController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\WebsiteValidation;
+use App\Http\Resources\WebsiteResource;
+use App\Models\Website;
+
+class WebsiteController extends Controller
+{
+    public function getList()
+    {
+        $websites = Website::all();
+
+        return [
+            'websites' => WebsiteResource::collection($websites)
+        ];
+    }
+
+    public function store(WebsiteValidation $request)
+    {
+        Website::create($request->validated());
+    }
+}
